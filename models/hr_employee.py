46c9e07cb33a0aa420af9f33b93e97efa4e11b2c
yshiwerekey-cmd <yshiwerekey@gmail.com>
2026-03-03 10:37:25 +0300

    Update employee evaluation type label

--- models/hr_employee.py
+++ models/hr_employee.py
@@ -6,7 +6,7 @@ class HREmployeeBase(models.AbstractModel):
     evaluation_ids = fields.One2many('emp.evaluation', 'employee_id', string='Evaluations')
     
     emp_eval_type = fields.Selection([
-        ('supervisor', 'Supervisor'),
+        ('supervisor', 'Supervisor/manager'),
         ('standard', 'Employee'),
     ], string="Employee Type", default='standard', tracking=True, required=True,)
 
