5fa10305ec84f9991df91a6ab2ce77fd7a79dcd5
yshiwerekey-cmd <yshiwerekey@gmail.com>
2026-03-19 09:46:44 +0300

    Fix rating_selection formatting in performance_evaluation

--- models/performance_evaluation.py
+++ models/performance_evaluation.py
@@ -82,7 +82,7 @@ class EmpEvaluation(models.Model):
         default='draft',
         tracking=True
     )
-    rating_selection = [('5', ' '), ('4', ' '), ('3', ' '), ('2', ' '), ('1', ' '), ('NA', ' ')]
+    rating_selection = [('5', '5)'), ('4', '4)'), ('3', '3)'), ('2', '2)'), ('1', '1)'), ('NA', 'NA)')]
     total_score = fields.Float(string="Total Score", compute="_compute_total_score", store=True)
 
 
