a5c563d32385a69f08542778ae703e94ab3769d4
Yohannes Shiwerekey <yshiwerekey@gmail.com>
2026-05-24 22:09:52 +0300

    Scope radio styles to .o_evaluation_form and define action_update_evaluation_groups to prevent impact on other modules

--- models/hr_employee.py
+++ models/hr_employee.py
@@ -13,5 +13,25 @@ class HREmployeeBase(models.AbstractModel):
 class HREmployee(models.Model):
     _inherit = 'hr.employee'
 
+    def action_update_evaluation_groups(self):
+        # Assign evaluation groups based on username matching keywords
+        employee_group = self.env.ref('evaluation_employee.group_evaluation_employee', raise_if_not_found=False)
+        coach_group = self.env.ref('evaluation_employee.group_evaluation_coach', raise_if_not_found=False)
+        hr_group = self.env.ref('evaluation_employee.group_evaluation_hr', raise_if_not_found=False)
+
+        for employee in self:
+            user = employee.user_id
+            if not user or user.login in ('admin', 'root'):
+                continue
+
+            login = (user.login or '').lower()
+            if 'coach' in login and coach_group and coach_group not in user.groups_id:
+                user.groups_id |= coach_group
+            elif 'hr' in login and hr_group and hr_group not in user.groups_id:
+                user.groups_id |= hr_group
+            elif employee_group and employee_group not in user.groups_id:
+                user.groups_id |= employee_group
+        return True
+
 class HREmployeePublic(models.Model):
     _inherit = 'hr.employee.public'
